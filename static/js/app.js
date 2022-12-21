9f84de33d9a9e16841c662dab91e70f0c51aadb4
parna.kundu@gmail.com <parna.kundu@gmail.com>
2022-12-21 18:58:16 -0500

    Add table filter criteria

--- static/js/app.js
+++ static/js/app.js
@@ -11,7 +11,7 @@ function buildTable(data) {
 
     // Next, loop through each object in the data
     // and append a row and cells for each value in the row
-    data.forEach((dataRow)) => {
+    data.forEach((dataRow) => {
         //Append a row to the table body
         let row = tbody.append("tr");
 
@@ -22,6 +22,29 @@ function buildTable(data) {
             cell.text(val);
         });
     });
-}
+};
 
+function handleClick() {
+    //Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
 
+    //Check to see if a date was entered and filter the 
+    //data using that date. 
+    if (date) {
+        //Apply 'filter' to the table data to only keep the 
+        //rows where the 'datetime' value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    //Rebuild the table using the filtered data
+    //@NOTE : If no data was entered, then filterData will 
+    //just be the original tableData.
+    buildTable(filteredData);
+};
+
+//Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+//Build the table when the page loads
+buildTable(tableData);
